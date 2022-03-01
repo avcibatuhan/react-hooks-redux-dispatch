bccc641301164af9c8eae3fac483324eacf10ce1
avcibatuhan <avcibatuhan26@gmail.com>
2022-03-01 10:51:49 +0300

    redux dispatch

--- src/reducer.js
+++ src/reducer.js
@@ -10,4 +10,16 @@ function changeState(state, action) {
 let state = { count: 0 };
 let action = { type: "counter/increment" };
 
-changeState(state, action);
+//changeState(state, action);
+function render() {
+  document.body.textContent = state.count;
+}
+function dispatch(action) {
+  state = changeState(state, action);
+  render();
+}
+render();
+//render()
+//dispatch(action)
+dispatch({ type: "counter/increment" });
+dispatch({ type: "counter/increment" }); 
